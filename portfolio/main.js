70f23c4fe27a5e8f33f1e4a1f184e626c9d55f07
Lui Jangulashvili <luijangulashvili@gmail.com>
2024-09-25 08:12:34 +0000

    edited contacts page

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+window.addEventListener('load', () => {
+    window.addEventListener('load', () => {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        link.href = '/portfolio/style.css'; // Adjust the path as needed
+        document.head.appendChild(link);
+    });
+});
